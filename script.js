efb162da48e3ad7b5a976c3be06237909f174fe7
Matt Glowacki <mattwglowacki@gmail.com>
2023-06-09 15:51:53 -0400

    Render forecast cards

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+getWeather('Philadelphia')
+
 async function getWeather(location) {
-  const forecastResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=c68a6f05871548fc8c405836232904&q=${location}&days=7`, {mode:'cors'})
+  const forecastResponse = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=c68a6f05871548fc8c405836232904&q=${location}&days=3`, {mode:'cors'})
   const forecastJSON = await forecastResponse.json()
 
   const weather = await parseForecast(forecastJSON)
 
-  renderDisplay(weather)
+  console.log("JSON", forecastJSON)
+  console.log("weather", weather)
 
-  console.log(weather)
+  renderDisplay(weather)
 }
 
+async function renderDisplay(weather) {
+  setCurrentDate(weather.current.date)
+  setCurrentValues(weather.current)
+  weather.forecast.forEach((day) => {
+    setForecastValues(day)
+  })
+}
+
+
 async function parseForecast(forecastJSON) {
-  console.log(forecastJSON)
   let weather = {current: {
                     date: forecastJSON.current.last_updated,
                     condition: forecastJSON.current.condition,
@@ -28,17 +39,12 @@ async function parseForecast(forecastJSON) {
     weather.forecast[i] = {}
 
     weather.forecast[i].date = day.date
-    weather.forecast[i].condition = day.condition
-    weather.forecast[i].feelslikeF = day.feelslike_f
-    weather.forecast[i].humidity = day.humidity
-    weather.forecast[i].cloud = day.cloud
-    weather.forecast[i].precip = day.precip
-    weather.forecast[i].temp = day.temp_f
-    weather.forecast[i].windSpd = day.wind_mph
-    weather.forecast[i].windDir = day.wind_dir
+    weather.forecast[i].condition = day.day.condition
+    weather.forecast[i].highTempF = day.day.maxtemp_f
+    weather.forecast[i].lowTempF = day.day.mintemp_f
+    weather.forecast[i].precip = Math.max(day.day.daily_chance_of_rain, day.day.daily_chance_of_snow)
+    weather.forecast[i].maxWindSpd = day.day.maxwind_mph
   })
-
-  console.log(weather)
 
   return weather
 }
@@ -52,17 +58,23 @@ formSubmit.addEventListener('click', (e) => {
   getWeather(location.value)
 })
 
-getWeather('Philadelphia')
+async function setCurrentDate(dateJSON) {
+  const span = document.querySelector('[data-current-date]')
+  const date = await new Date(dateJSON)
 
-async function renderDisplay(weather) {
-  console.log(weather.current)
-  setCurrentValues(weather.current)
-  weather.forecast.forEach((day) => {
-    setForecastValues(day)
-  })
+  let options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  };
+
+  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date)
+
+  span.textContent = formattedDate
 }
 
 async function setCurrentValues(weather) {
+  setValue('current-icon', weather.condition.icon)
   setValue('temp', weather.temp)
   setValue('feels-like', weather.feelslikeF)
   setValue('humidity', weather.humidity)
@@ -75,10 +87,55 @@ async function setCurrentValues(weather) {
 async function setValue(dataAttribute, dataValue) {
   const span = document.querySelector(`[data-${dataAttribute}]`)
 
-  console.log(span, dataValue)
-  span.textContent = dataValue
+  if (dataAttribute.includes('icon')) {
+    span.src = dataValue
+  } else {
+    span.textContent = dataValue
+  }
 }
 
-function setForecastValues() {
+async function setForecastValues(forecast) {
+  console.log("forecast day", forecast)
+  const forecastSection = document.querySelector(".forecast")
+  forecastSection.innerHTML = ''
+  const date = await new Date(forecast.date)
+  let options = {
+    weekday: "long",
 
+  };
+  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date)
+  const icon = await forecast.condition.icon
+  const conditionText = await forecast.condition.text
+  const hiTemp = await forecast.highTempF
+  const lowTemp = await forecast.lowTempF
+  const precip = await forecast.precip
+  const windSpd = await forecast.maxWindSpd
+  const windDir = await forecast.maxWindDir
+
+  const forecastCard = `
+  <div class="forecast-card">
+    <div class="datum">
+      <div class="date">${formattedDate}</div>
+    </div>
+    <div class="datum"><img src=${icon}></div>
+    <div class="datum">${conditionText}</div>
+    <div class="datum">
+      <div class="datum-title">High Temp</div>
+      <div class="datum-value"><span class="value">${hiTemp}</span><span class="unit">F</span></div>
+    </div>
+    <div class="datum">
+      <div class="datum-title">Low Temp</div>
+      <div class="datum-value"><span class="value">${lowTemp}</span><span class="unit">F</span></div>
+    </div>
+    <div class="datum">
+      <div class="datum-title">Precip</div>
+      <div class="datum-value"><span class="value">${precip}</span><span class="unit">%</span></div>
+    </div>
+    <div class="datum">
+      <div class="datum-title">Wind Spd</div>
+      <div class="datum-value"><span class="value">${windSpd}</span><span class="unit">mph</span></div>
+    </div>
+  </div>`
+
+  forecastSection.innerHTML += forecastCard
 }
